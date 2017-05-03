51d514fef04b6e1a09a65a6dfda429b475106694
pkshrestha pradhyu.shrestha@gmail.com <pradhyu.shrestha@gmail.com>
2017-05-02 22:44:56 -0400

    solution

--- CourseraScalaCourse2/calculator/src/main/scala/calculator/Calculator.scala
+++ CourseraScalaCourse2/calculator/src/main/scala/calculator/Calculator.scala
@@ -1,28 +1,47 @@
 package calculator
 
 sealed abstract class Expr
+
 final case class Literal(v: Double) extends Expr
+
 final case class Ref(name: String) extends Expr
+
 final case class Plus(a: Expr, b: Expr) extends Expr
+
 final case class Minus(a: Expr, b: Expr) extends Expr
+
 final case class Times(a: Expr, b: Expr) extends Expr
+
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    val returnExp = namedExpressions.map({ item =>
+      val name= item._1
+      val exp=item._2
+      (name, Signal(eval(exp(), namedExpressions)))
+    })
+    returnExp
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => {
+        val ref = getReferenceExpr(name, references)
+        eval(ref, references - name)
+      }
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    }
   }
 
   /** Get the Expr for a referenced variables.
-   *  If the variable is not known, returns a literal NaN.
-   */
-  private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+    * If the variable is not known, returns a literal NaN.
+    */
+  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]) = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
--- CourseraScalaCourse2/calculator/src/main/scala/calculator/Polynomial.scala
+++ CourseraScalaCourse2/calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,24 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] = {
+    // Δ = b² - 4ac
+    Signal(math.pow(b(), 2D) - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    //(-b ± √Δ) / 2a
+
+    val delta: Signal[Double] = computeDelta(a, b, c)
+    if (delta() < 0)
+      Signal(Set())
+    else {
+      val deltaSqrt = math.sqrt(delta())
+      val firstSoln = (-b() + deltaSqrt) / (2 * a())
+      val secondSoln = (-b() - deltaSqrt) / (2 * a())
+      val solns: Set[Double] = Set[Double](firstSoln) + secondSoln
+      Signal(solns)
+    }
   }
 }
